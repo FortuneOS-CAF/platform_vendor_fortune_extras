9f058dfa6c364d184eb14bafa683a36ae61b8710
Pranav Vashi <neobuddy89@gmail.com>
2024-04-12 13:09:12 -0700

    extras: Import fonts from crDroid
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: KuroeHanako <78864678+kuroehanako@users.noreply.github.com>
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Kizziama <kizziama@proton.me>
    Change-Id: I3514b69d48d760bc27a56e2a724d4e3b7d5299cb

--- config.mk
+++ config.mk
@@ -10,6 +10,52 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # Bootanimation
 include vendor/fortune/extras/bootanimation/bootanimation.mk
 
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontAclonicaOverlay \
+    FontAmaranteOverlay \
+    FontBariolOverlay \
+    FontCagliostroOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCookieRunOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontFifa2018Overlay \
+    FontGrandHotelOverlay \
+    FontHarmonySansOverlay \
+    FontLatoOverlay \
+    FontLinotteOverlay \
+    FontNokiaPureOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontNothingDotOverlay \
+    FontNunitoOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontOswaldOverlay \
+    FontPlayOverlay \
+    FontQuandoOverlay \
+    FontRedressedOverlay \
+    FontReemKufiOverlay \
+    FontRobotoCondensedOverlay \
+    FontRubikOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSamsungOneOverlay \
+    FontSansSerifOverlay \
+    FontSonySketchOverlay \
+    FontStoropiaOverlay \
+    FontSurferOverlay \
+    FontUbuntuOverlay
+
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/fortune/extras/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
 # Overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/fortune/extras/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune/extras/overlay
