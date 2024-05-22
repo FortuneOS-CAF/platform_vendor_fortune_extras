de1039b19f144f03362a23b1f52837e750350ae4
Kizziama <kizziama@proton.me>
2024-05-22 03:25:03 +0000

    extras: Import our default wallpaper
    Co-authored-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>
    Change-Id: I0f666a3af3d4cc0c1f4a75d0a09e2b936be7650b

--- config.mk
+++ config.mk
@@ -9,3 +9,7 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 # Bootanimation
 include vendor/fortune/extras/bootanimation/bootanimation.mk
+
+# Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/fortune/extras/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune/extras/overlay
